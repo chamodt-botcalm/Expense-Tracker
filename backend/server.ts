10e93e01007f2cfd7b153f4722eb3521cc2b4130
Chamod Theekshana <chamod.t@botcalm.com>
2025-12-25 23:49:45 +0530

    feat: Transaction API Created

--- backend/server.ts
+++ backend/server.ts
@@ -26,18 +26,34 @@ async function initDB() {
     }
 }
 
-app.post("/api.transactions",(req,res)=>{
-    try {
-        const {title, amount, category, user_id} = req.body;
-    } catch (error) {
-        
-    }
-}
-)
+app.get("/api/transactions", async (req, res) => {
+    res.send('API is running...');
+});
 
-initDB().then(()=>
- app.listen(PORT, () => {
-    console.log("SERVER IS UP AND RUNNING ON PORT:", PORT);
-})
+app.post("/api/transactions", async (req, res) => {
+    try {
+        const { title, amount, category, user_id } = req.body;
+
+        if (!title || !amount || !category || !user_id) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        const result = await sql`
+            INSERT INTO transactions (title, amount, category, user_id)
+            VALUES (${title}, ${amount}, ${category}, ${user_id})
+            RETURNING *
+        `;
+        console.log("Transaction created:", result);
+        res.status(201).json({ message: "Transaction created successfully", transaction : result[0] });
+    } catch (error) {
+        console.log("Error creating transaction:", error);
+        res.status(500).json({ message: "Server Error" });
+    }
+});
+
+initDB().then(() =>
+    app.listen(PORT, () => {
+        console.log("SERVER IS UP AND RUNNING ON PORT:", PORT);
+    })
 )
 
