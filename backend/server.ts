e94ec702935bf2ffec6f4c253d50d804e7c2f995
Adeepa-s <chamod.t@botcalm.com>
2025-12-26 14:47:31 +0530

    feat: created api endpoints

--- backend/server.ts
+++ backend/server.ts
@@ -26,11 +26,11 @@ async function initDB() {
     }
 }
 
-app.get("/api/transactions", async (req, res) => {
+app.get("/api/transaction", async (req, res) => {
     res.send('API is running...');
 });
 
-app.post("/api/transactions", async (req, res) => {
+app.post("/api/transaction", async (req, res) => {
     try {
         const { title, amount, category, user_id } = req.body;
 
@@ -44,12 +44,60 @@ app.post("/api/transactions", async (req, res) => {
             RETURNING *
         `;
         console.log("Transaction created:", result);
-        res.status(201).json({ message: "Transaction created successfully", transaction : result[0] });
+        res.status(201).json({ message: "Transaction created successfully", transaction: result[0] });
     } catch (error) {
         console.log("Error creating transaction:", error);
         res.status(500).json({ message: "Server Error" });
     }
 });
+
+app.get("/api/transaction:user_id", async (req, res) => {
+    try {
+        const transactions = await sql`SELECT * FROM transactions WHERE user_id = ${req.params.user_id} ORDER BY created_at DESC`;
+        res.status(200).json({ message: "Transactions fetched successfully", transactions });
+    }
+    catch (error) {
+        res.status(500).json({ message: "Server Error" });
+    } ``
+});
+app.delete("/api/transaction/:id", async (req, res) => {
+    try {
+        const result = await sql`DELETE FROM transactions WHERE id = ${req.params.id} RETURNING *`;
+
+        if(isNaN(Number(req.params.id))) {
+            return res.status(400).json({ message: "Invalid transaction ID" });
+        }
+
+        if (result.length === 0) {
+            return res.status(404).json({ message: "Transaction not found" });
+        }
+        
+            res.status(200).json({ message: "Transaction deleted successfully" });
+        
+    }
+    catch (error) {
+        return res.status(500).json({ message: "Server Error" });
+    }
+
+});
+
+app.get("/api/transaction/summary/:user_id", async (req, res) => {
+    try {
+        const balanceResult = await sql`SELECT COALESCE(SUM(amount), 0) AS balance FROM transactions WHERE user_id = ${req.params.user_id}`;
+        const incomeResult = await sql`SELECT COALESCE(SUM(amount), 0) AS income FROM transactions WHERE user_id = ${req.params.user_id} AND amount > 0`;
+        const expenseResult = await sql`SELECT COALESCE(SUM(amount), 0) AS expense FROM transactions WHERE user_id = ${req.params.user_id} AND amount < 0`;
+        
+        res.status(200).json({
+            balance: balanceResult[0].balance,
+            income: incomeResult[0].income,
+            expense: expenseResult[0].expense
+        });
+    }
+    catch (error) {
+        res.status(500).json({ message: "Server Error" });
+    }
+});
+
 
 initDB().then(() =>
     app.listen(PORT, () => {
